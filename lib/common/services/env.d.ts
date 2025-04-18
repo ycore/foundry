9af538b6f63c3de1952370727c20692c2059276e
Johan Meyer <y-core@outlook.com>
2025-04-18 14:47:30 +0200

    Update ThemeSwitcher

--- lib/common/services/env.d.ts
+++ lib/common/services/env.d.ts
@@ -1,6 +1,19 @@
 import type { AppLoadContext } from 'react-router';
-export declare function validateEnvironment(context: AppLoadContext): void;
-export declare function contextEnv(context: AppLoadContext): Env;
+export interface InitConfig {
+    env?: {
+        exclude?: string[];
+    };
+    database?: Array<{
+        binding: string;
+        tables: string[];
+    }>;
+}
+export declare const getInitState: () => {
+    completed: boolean;
+    passed: boolean;
+};
 export declare const isDev: (context: AppLoadContext) => boolean;
 export declare const isProduction: (context: AppLoadContext) => boolean;
 export declare const isTesting: (context: AppLoadContext) => boolean;
+export declare function contextEnv(context: AppLoadContext): Env;
+export declare function validateEnvironment(env: Env, init: InitConfig): Promise<void>;
